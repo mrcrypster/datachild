cf643d32cb64ac9999ce5f77455c66da53e142df
mrcrypster <mr.crypster@gmail.com>
2023-04-20 12:39:34 +0300

    deployment - clear cache & sitemap

--- scripts/deploy.php
+++ scripts/deploy.php
@@ -10,3 +10,11 @@ passthru('git pull');
 # compile CSS
 $web = realpath(__DIR__ . '/../web');
 passthru("lessc {$web}/s.less {$web}/s.css");
+
+
+# remove cache
+exec('rm -rf /var/cache/nginx/datachild/*');
+
+
+# rebuild sitemaps
+include __DIR__ . '/sitemap.php';
